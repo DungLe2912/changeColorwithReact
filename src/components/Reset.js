9405e98045aaf6ea8b8d7c1f499730b4ec292353
DungLe2912 <qdung.le3912@gmail.com>
2020-03-24 21:38:47 +0700

    complete reset default function

--- src/components/Reset.js
+++ src/components/Reset.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
 class Reset extends Component {
+    onReset(value){
+        this.props.onReset(value);
+    }
     render() {
         return (
-            <button type="button" className="btn btn-primary">reset</button>
+            <button type="button" className="btn btn-primary" onClick={()=>this.onReset(true)}>reset</button>
         );
     }
 }
